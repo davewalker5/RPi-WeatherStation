784850cbe7ec423bed22435387719b23b118534b
Dave Walker <david.walker5@icloud.com>
2025-11-11 16:47:25 +0000

    Update BME test script configuration

---
+++ scripts/test-bme.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+PROJECT_FOLDER=$( cd "$( dirname "$0" )/.." && pwd )
+. "$PROJECT_FOLDER/scripts/config.sh"
+
+python3 "$PROJECT_FOLDER/src/main/test--chip.py"
--- src/main/test-bme-chip.py
+++ src/main/test-bme-chip.py
@@ -1,28 +1,8 @@
 from smbus2 import SMBus
-import time
+from os import environ
 
-# Before running this script, run the following command:
-#
-# sudo i2cdetect -y <bus-number>
-#
-# Try values for 0 and 1 for the <bus-number>. One of them will return an error and the 
-# other will return output similar to the following:
-#
-#      0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f                                                                                                                                 
-# 00:                         -- -- -- -- -- -- -- --                                                                                                                                 
-# 10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --                                                                                                                                 
-# 20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --                                                                                                                                 
-# 30: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --                                                                                                                                 
-# 40: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --                                                                                                                                 
-# 50: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --                                                                                                                                 
-# 60: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --                                                                                                                                 
-# 70: -- -- -- -- -- -- 76 --   
-#
-# Only one entry will appear. The bus number and that entry should be used to populate the
-# bus number and address, below.
-
-bus_num = 0
-addr = 0x76
+bus_num = environ["BUS_NUMBER"]
+addr = environ["ADDR"]
 
 bus = SMBus(bus_num)
 chip_id = bus.read_byte_data(addr, 0xD0)
