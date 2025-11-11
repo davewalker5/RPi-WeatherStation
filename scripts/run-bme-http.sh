cdb99867bcb0c4a18847d468b618eff80f946562
Dave Walker <david.walker5@icloud.com>
2025-11-11 17:07:29 +0000

    Refactor the HTTP Server

--- scripts/run-bme-http.sh
+++ scripts/run-bme-http.sh
@@ -3,7 +3,7 @@
 PROJECT_FOLDER=$( cd "$( dirname "$0" )/.." && pwd )
 . "$PROJECT_FOLDER/scripts/config.sh"
 
-python3 "$PROJECT_FOLDER/src/main/bme280-http.py" \
+python3 "$PROJECT_FOLDER/src/main/bme-http.py" \
     --bus $BUS_NUMBER \
     --addr $ADDR \
     --db "$PROJECT_FOLDER/data/bme280.db" \
