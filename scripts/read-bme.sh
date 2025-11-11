891dc7f024b186ac655d5ed833c617d514e1c9fc
Dave Walker <david.walker5@icloud.com>
2025-11-11 15:51:53 +0000

    Fixup read bme script

--- scripts/read-bme.sh
+++ scripts/read-bme.sh
@@ -4,6 +4,8 @@ PROJECT_FOLDER=$( cd "$( dirname "$0" )/.." && pwd )
 BUS_NUMBER=0
 ADDR=0x76
 
+export PYTHONPATH="$PROJECT_FOLDER/src"
+
 python3 "$PROJECT_FOLDER/src/main/read-bme.py" \
     --bus $BUS_NUMBER \
     --addr $ADDR
