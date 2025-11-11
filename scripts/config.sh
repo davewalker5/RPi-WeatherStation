8ff92c3c6751fccca8e626d19d8a99b018c44f08
Dave Walker <david.walker5@icloud.com>
2025-11-11 16:19:41 +0000

    Add debugging output to config script

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,5 +2,11 @@
 
 PROJECT_FOLDER=$( cd "$( dirname "$0" )/.." && pwd )
 export PYTHONPATH="$PROJECT_FOLDER/src"
-export BUS=BUS_NUMBER=0
+export BUS_NUMBER=0
 export ADDR=0x76
+
+echo
+echo "Project Root : $PROJECT_ROOT"
+echo "Bus Bymber   : $BUS_NUMBER"
+echo "Address      : $ADDR"
+echo
