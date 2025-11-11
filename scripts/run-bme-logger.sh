4ade6489ac0809515b1f9b515bff2fda695a6d17
Dave Walker <david.walker5@icloud.com>
2025-11-11 14:44:39 +0000

    BME logger refactoring

--- scripts/run-bme-logger.sh
+++ scripts/run-bme-logger.sh
@@ -4,7 +4,7 @@ PROJECT_FOLDER=$( cd "$( dirname "$0" )/.." && pwd )
 BUS_NUMBER=0
 ADDR=0x76
 
-python3 "$PROJECT_FOLDER/src/bme280-logger.py" \
+python3 "$PROJECT_FOLDER/src/main/bme280-logger.py" \
     --bus $BUS_NUMBER \
     --addr $ADDR \
     --db "$PROJECT_FOLDER/data/bme280.db" \
