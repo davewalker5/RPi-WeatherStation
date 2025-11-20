5b55954f0394fc387fb9b0a6892ba0a6ed281988
Dave Walker <david.walker5@icloud.com>
2025-11-20 10:27:01 +0000

    Added mock SMBus tests

--- tests/unit/test_mock_smbus.py
+++ tests/unit/test_mock_smbus.py
@@ -1,3 +1,4 @@
+import pytest
 from helpers import MockSMBus, BME280_TRIMMING_PARAMETERS
 
 def test_read_trimming_parameters():
@@ -5,3 +6,12 @@ def test_read_trimming_parameters():
     for register, expected in BME280_TRIMMING_PARAMETERS.items():
         actual = bus.read_byte_data(None, register)
         assert actual == expected
+
+
+@pytest.mark.parametrize("fixture", [
+    [85, 28, 112, 125, 93, 240, 142, 35]
+])
+def test_read_i2c_block_data(fixture):
+    bus = MockSMBus(BME280_TRIMMING_PARAMETERS, fixture)
+    actual = bus.read_i2c_block_data(None, None, len(fixture))
+    assert actual == fixture
