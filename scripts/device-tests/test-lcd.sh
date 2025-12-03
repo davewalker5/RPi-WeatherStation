39081b276bf16ba439a445abdf084bf44eb8c284
Dave Walker <david.walker5@icloud.com>
2025-12-03 19:15:17 +0000

    Added LCD test script

--- scripts/device-tests/test-lcd.sh
+++ scripts/device-tests/test-lcd.sh
@@ -4,6 +4,4 @@ SCRIPT_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
 PROJECT_FOLDER=$( cd "$( dirname "$SCRIPT_PATH" )/../.." && pwd )
 . "$PROJECT_FOLDER/scripts/config.sh"
 
-python3 "$PROJECT_FOLDER/tests/device/test-lcd.py" \
-    --bus $BUS_NUMBER \
-    --bme-addr $BME_ADDR
+python3 "$PROJECT_FOLDER/tests/device/test-lcd.py"
