0a638aade0cd0b63c0faba528147a3a3bb3fa84d
Dave Walker <david.walker5@icloud.com>
2025-12-08 07:20:20 +0000

    Utility script to query latest readings from the DB

---
+++ scripts/query-db.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+SCRIPT_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
+PROJECT_FOLDER=$( cd "$( dirname "$SCRIPT_PATH" )/.." && pwd )
+. "$PROJECT_FOLDER/scripts/config.sh"
+
+# Capture the path to the database
+DB_PATH="$1"
+
+# Define the API queries to run
+queries=(
+  "query-bme"
+  "query-veml"
+  "query-sgp"
+)
+
+# Iterate over the endpoints
+for i in "${!queries[@]}"; do
+    query_file="$PROJECT_FOLDER/sql/${queries[$i]}.sql"
+    echo
+    sqlite2 "$DB_PATH" "$query_file"
+    echo
+done
+
+echo
