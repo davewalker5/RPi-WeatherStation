d1cef6b98a1ff5337fe2f0fe2a3edbd5ee5482df
Dave Walker <david.walker5@icloud.com>
2025-12-08 08:54:19 +0000

    Prepare to run dashboard as a module

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -8,12 +8,6 @@ load_dotenv()
 
 app = Flask(__name__)
 
-WEATHER_SCHEME = os.getenv("WEATHER_SCHEME", "http")
-WEATHER_HOST = os.getenv("WEATHER_HOST", "localhost")
-WEATHER_PORT = os.getenv("WEATHER_PORT", "80")
-WEATHER_API_URL = f"{WEATHER_SCHEME}://{WEATHER_HOST}:{WEATHER_PORT}/api/current"
-
-
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -21,19 +15,25 @@ def index():
 
 @app.route("/api/current")
 def current_weather():
+    WEATHER_SCHEME = os.getenv("WEATHER_SCHEME")
+    WEATHER_HOST = os.getenv("WEATHER_HOST")
+    WEATHER_PORT = os.getenv("WEATHER_PORT")
+    WEATHER_API_BASE_URL = f"{WEATHER_SCHEME}://{WEATHER_HOST}:{WEATHER_PORT}/api"
+    print(WEATHER_API_BASE_URL)
+
     try:
         # Get the latest BME280 readings
-        resp = requests.get("http://weatherpi.local/api/bme", timeout=3)
+        resp = requests.get(f"{WEATHER_API_BASE_URL}/bme", timeout=3)
         resp.raise_for_status()
         bme_readings = resp.json()
 
         # Get the latest VEML7700 readings
-        resp = requests.get("http://weatherpi.local/api/veml", timeout=3)
+        resp = requests.get(f"{WEATHER_API_BASE_URL}/veml", timeout=3)
         resp.raise_for_status()
         veml_readings = resp.json()
 
         # Get the latest SGP40 readings
-        resp = requests.get("http://weatherpi.local/api/sgp", timeout=3)
+        resp = requests.get(f"{WEATHER_API_BASE_URL}/sgp", timeout=3)
         resp.raise_for_status()
         sgp_readings = resp.json()
 
@@ -44,15 +44,15 @@ def current_weather():
             "sgp": sgp_readings
         }
 
-    except Exception:
-        return jsonify({"error": "Failed to contact weather station"}), 502
+    except Exception as e:
+        return jsonify({"error": e}), 502
 
     return jsonify(data)
 
+def main():
+    DASHBOARD_PORT = os.getenv("DASHBOARD_PORT")
+    app.run(host="0.0.0.0", port=DASHBOARD_PORT, debug=True)
+
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=int(os.getenv("FLASK_PORT", 5000)),
-        debug=os.getenv("FLASK_DEBUG") == "1",
-    )
+    main()
