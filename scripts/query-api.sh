1bda1b7262ec0ec3c5b2854edb035e70d8cd31f3
Dave Walker <david.walker5@icloud.com>
2025-12-08 07:10:58 +0000

    Ability to override host/port in API query script

--- scripts/query-api.sh
+++ scripts/query-api.sh
@@ -4,6 +4,25 @@ SCRIPT_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
 PROJECT_FOLDER=$( cd "$( dirname "$SCRIPT_PATH" )/.." && pwd )
 . "$PROJECT_FOLDER/scripts/config.sh"
 
+# Parse the arguments, if any, to override default hostname and port
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --host)
+      PI_HOSTNAME="$2"
+      shift 2
+      ;;
+    --port)
+      PI_PORT="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
+
+
 # Define the API endpoints to be tested
 endpoints=(
   "health"
