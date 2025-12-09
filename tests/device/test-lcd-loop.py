f0225ae5416ce7c7af920a38312857ef9fb80015
Dave Walker <david.walker5@icloud.com>
2025-12-09 07:26:07 +0000

    Add looping test script for the LCD

---
+++ tests/device/test-lcd-loop.py
@@ -0,0 +1,17 @@
+import smbus
+from time import sleep
+from os import environ
+from i2c_lcd import I2CLCD
+
+bus_num = int(environ["BUS_NUMBER"])
+addr = int(environ["LCD_ADDR"], 16)
+
+bus = smbus.SMBus(bus_num)
+lcd = I2CLCD(bus, addr)
+
+i = 0
+while True:
+    lcd.write(f"Count: {i}", line=1)
+    lcd.write("Weather OK   ", line=2)
+    i += 1
+    sleep(1)
