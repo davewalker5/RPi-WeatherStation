cf7fd73afac84c040fb9d7b470026e5d1f1fb15d
Dave Walker <david.walker5@icloud.com>
2025-12-16 19:14:26 +0000

    Fix arguments to VEML logger script

--- scripts/run-veml-logger.sh
+++ scripts/run-veml-logger.sh
@@ -5,9 +5,5 @@ PROJECT_FOLDER=$( cd "$( dirname "$SCRIPT_PATH" )/.." && pwd )
 . "$PROJECT_FOLDER/scripts/config.sh"
 
 python3 "$PROJECT_FOLDER/src/main/veml-logger.py" \
-    --bus $BUS_NUMBER \
-    --veml-addr $VEML_ADDR \
-    --veml-gain $VEML_GAIN \
-    --veml-integration-ms $VEML_INTEGRATION_TIME \
     --db "$PROJECT_FOLDER/data/weather.db" \
     "$@"
