d6cf0890ee01714c70f5c15410edcb9c506a1301
Dave Walker <david.walker5@icloud.com>
2025-12-19 11:26:48 +0000

    Added LCD display tests

--- src/service/__init__.py
+++ src/service/__init__.py
@@ -3,11 +3,13 @@ from .sampler import Sampler
 from .bme280_sampler import BME280Sampler
 from .veml7700_sampler import VEML7700Sampler
 from .sgp40_sampler import SGP40Sampler
+from .lcd_display import LCDDisplay
 
 __all__ = [
     "RequestHandler",
     "Sampler",
     "BME280Sampler",
     "VEML7700Sampler",
-    "SGP40Sampler"
+    "SGP40Sampler",
+    "LCDDisplay"
 ]
